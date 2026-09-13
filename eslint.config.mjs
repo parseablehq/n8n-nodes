ccfffd2ec3d956bcf9ef3554201f5ba6db9020e1
Praveen K B <praveenbhat98@gmail.com>
2026-09-13 11:23:50 +0530

    chore: allow AGPL package license

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,11 @@
 import { config } from '@n8n/node-cli/eslint';
 
-export default config;
+export default [
+	...config,
+	{
+		files: ['package.json'],
+		rules: {
+			'n8n-nodes-base/community-package-json-license-not-default': 'off',
+		},
+	},
+];
